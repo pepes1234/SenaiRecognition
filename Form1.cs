a4b9dc742ab904d6098315a4b0fb811835c8a1ce
pepes1234 <103346831+pepes1234@users.noreply.github.com>
2022-09-29 16:02:35 -0300

    Inicialização base do Windows forms

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,20 @@
 namespace SenaiRecognition;
+using System.IO;
 
 public partial class Form1 : Form
 {
-    
+    PictureBox pb1;
     public Form1()
     {
         InitializeComponent();
+        Load += delegate
+        {
+            pb1 = new PictureBox();
+            pb1.Image = Image.FromFile(@"TestImages\test1(64x64).png");
+            pb1.Dock = DockStyle.Fill;
+            pb1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(pb1);
+        };
     }
+    
 }
